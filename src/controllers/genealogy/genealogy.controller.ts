0fd0af45d6e907ba15fcbf2e55f5a471e4a209ee
unknown <willyumburjer@gmail.com>
2020-01-23 15:26:09 +0800

    Genealogy get Filter done

--- src/controllers/genealogy/genealogy.controller.ts
+++ src/controllers/genealogy/genealogy.controller.ts
@@ -3,12 +3,68 @@ import { Request, Response, NextFunction } from "express";
 import { Controller } from "@mayajs/core";
 import { GenealogyServices } from "./genealogy.service";
 
+const { verifyTokenMember } = require("../../middleware/index");
+const _ = require("lodash");
+
 @Controller({
   model: "./genealogy.model",
   route: "/genealogy"
 })
 export class GenealogyController {
   constructor(private services: GenealogyServices) {}
+
+  @Get({ path: "/", middlewares: [verifyTokenMember] })
+  async getFilter(req: Request, res: Response, next: NextFunction) {
+    function removeDuplicates(array: any) {
+      let x: any = {};
+      array.forEach(function(i: any) {
+        if (!x[i]) {
+          x[i] = true;
+        }
+      });
+      return Object.keys(x);
+    }
+
+    // const userId = req.body.user.memberId;
+    const userId = "QCY214337";
+    const result = await this.services.getFilter(userId);
+
+    let dates: any = [];
+    let period: any = [];
+    let partialFilter: any = JSON.stringify(result.data);
+    let b2bfilter = JSON.parse(partialFilter);
+
+    //PickedFilter is rawfile picked
+    const pickedFilter = b2bfilter.map((el: any) => {
+      const picked = _.pick(el, ["DateProcessed", "PERIODNO"]);
+      return picked;
+    });
+
+    pickedFilter.forEach((el: any) => {
+      dates.push(el.DateProcessed);
+      period.push(el.PERIODNO);
+    });
+
+    const filteredDate = removeDuplicates(dates);
+
+    const filteredPeriod = removeDuplicates(period);
+
+    var sortedStrings = filteredDate.sort(function(a: any, b: any) {
+      var aComps = a.split("/");
+      var bComps = b.split("/");
+      var aDate = new Date(aComps[2], aComps[0], aComps[1]);
+      var bDate = new Date(bComps[2], bComps[0], bComps[1]);
+      return aDate.getTime() - bDate.getTime();
+    });
+
+    res.status(result.status).json({
+      message: result.message,
+      filter: {
+        dateInterval: sortedStrings,
+        periodInterval: filteredPeriod
+      }
+    });
+  }
   // constructor(private services: SampleServices) {}
 
   // // This is a GET request equal to "/sample"
--- src/controllers/genealogy/genealogy.service.ts
+++ src/controllers/genealogy/genealogy.service.ts
@@ -1,8 +1,74 @@
 import { Injectable } from "@mayajs/core";
 import { Models } from "@mayajs/mongo";
+const _ = require("lodash");
 
 @Injectable()
 export class GenealogyServices {
   @Models("genealogy") model: any;
   constructor() {}
+
+  async getFilter(userId: any) {
+    const id = userId;
+    try {
+      /* function removeDuplicates(array: any) {
+        let x: any = {};
+        array.forEach(function(i: any) {
+          if (!x[i]) {
+            x[i] = true;
+          }
+        });
+        return Object.keys(x);
+      } */
+
+      const partialFilter = await this.model.find({ MemberID: id });
+
+      /*       console.log(typeof partialFilter);
+
+      let dates: any = [];
+      let period: any = [];
+      // let data: any;
+      //PickedFilter is rawfile picked
+      const pickedFilter = partialFilter.map((el: any) => {
+        
+        const picked = _.pick(el, ["DateProcessed", "PERIODNO"]);
+        return picked;
+
+      });
+      console.log(pickedFilter);
+
+      pickedFilter.forEach((el: any) => {
+        dates.push(el.DateProcessed);
+        period.push(el.PERIODNO);
+      });
+
+
+
+      const filteredDate = removeDuplicates(dates);
+
+      const filteredPeriod = removeDuplicates(period);
+
+      var sortedStrings = filteredDate.sort(function(a: any, b: any) {
+        var aComps = a.split("/");
+        var bComps = b.split("/");
+        var aDate = new Date(aComps[2], aComps[0], aComps[1]);
+        var bDate = new Date(bComps[2], bComps[0], bComps[1]);
+        return aDate.getTime() - bDate.getTime();
+      }); */
+      // console.log(sortedStrings);
+
+      return {
+        status: 200,
+        message: "Genealogy Fetch by id",
+        data: partialFilter,
+        meta: {}
+      };
+    } catch (error) {
+      return {
+        status: 400,
+        message: error.errmsg ? error.errmsg : error.toString(),
+        data: [],
+        meta: {}
+      };
+    }
+  }
 }
