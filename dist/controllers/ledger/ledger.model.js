e455ad47a3c6d3aed0a9ba3848f234b650cd0e47
unknown <willyumburjer@gmail.com>
2020-02-04 17:02:43 +0800

    Second Try

---
+++ dist/controllers/ledger/ledger.model.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongo_1 = require("@mayajs/mongo");
+const schema = mongo_1.MongoSchema({
+    MemberID: {
+        type: String
+    }
+});
+exports.default = mongo_1.MongoModel("Ledger", schema);
